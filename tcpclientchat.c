c86e7cbd203f9b6adf17eb0ec651153bf45571f8
Allison Bolen <bolena@dc18.cis.gvsu.edu>
2018-02-15 22:11:15 -0500

    working transfer, broken quit

--- tcpclientchat.c
+++ tcpclientchat.c
@@ -2,6 +2,8 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/select.h>
+#include <unistd.h>
 
 int main(int argc, char** argv){
 
@@ -11,14 +13,23 @@ int main(int argc, char** argv){
   fgets(ip,20,stdin);
   printf("What is the port?\n");
   fgets(port,6,stdin);
+
+
   int ports = atoi(port);
   int sockfd = socket(AF_INET,SOCK_STREAM,0);
+
   if(sockfd<0){
     printf("There was an error creating the socket\n");
     return 1;
   }
 
-  struct sockaddr_in serveraddr;
+
+  fd_set sockets;
+  FD_ZERO(&sockets);
+  FD_SET(sockfd,&sockets);
+  FD_SET(STDIN_FILENO, &sockets);
+
+  struct sockaddr_in serveraddr, clientaddr;
   serveraddr.sin_family=AF_INET;
   serveraddr.sin_port=htons(ports);
   serveraddr.sin_addr.s_addr=inet_addr(ip);
@@ -28,15 +39,41 @@ int main(int argc, char** argv){
     printf("There was an error connecting\n");
     return 1;
   }
+  printf("Please enter a message for the server:\n");
+
 while(1){
-   printf("Enter a line: ");
-   char line[5000];
-   char line2[5000];
-   fgets(line,5000,stdin);
-   send(sockfd,line,strlen(line)+1,0);
-   recv(sockfd,line2,5000,0);
-   printf("%s\n",line2);
-   close(sockfd);
+
+    int len=sizeof(clientaddr);
+  	fd_set tmp_set = sockets;
+  	select(FD_SETSIZE,&tmp_set, NULL, NULL, NULL);
+  	int i;
+
+  	for(i=0; i<FD_SETSIZE;i++){
+  		if(FD_ISSET(i,&tmp_set)){
+            if(i == STDIN_FILENO){
+  			  char input[5000];
+  			  fgets(input,5000,stdin);
+  			  printf("\nSending to server: %s",input);
+  			  send(sockfd,input,strlen(input)+1,0);
+  			  if(strcmp(input, "quit")==0){
+  			    close(i);
+  			    FD_CLR(i,&sockets);
+  			    return 0;
+  			  }
+            }
+  			else if(i==sockfd){
+              char line[5000];
+  		      recv(i,line,5000,0);
+  			  printf("Got from server: %s\n",line);
+  			  printf("size: %d",strlen(line));
+  			  if(strcmp(line, "quit")==0){
+                 close(i);
+                 FD_CLR(i,&sockets);
+                 return 0;
+              }
+  			}
+  		}
+  	}
  }
 return 0;
 } 
--- tcpserverchat.c
+++ tcpserverchat.c
@@ -10,10 +10,11 @@ int main(int argc, char **argv){
   //file descriptor
   fd_set sockets;
   FD_ZERO(&sockets);
-  
+  char trash[10];
   int port;
   printf("What is the port to use for the server?\n");
   scanf("%d", &port);
+  fgets(trash,10,stdin);
 
   struct sockaddr_in serveraddr,clientaddr;
   serveraddr.sin_family=AF_INET;
@@ -24,8 +25,8 @@ int main(int argc, char **argv){
   listen(sockfd,10);
   FD_SET(sockfd, &sockets);
   FD_SET(STDIN_FILENO, &sockets);
-  printf("\nPlease enter your message:\n");
-  int clientsocket = accept(sockfd,(struct sockaddr*)&clientaddr,&len); 
+  printf("\nPlease enter a message for the client:\n");
+  int clientsocket;
  while(1){
     
     int len=sizeof(clientaddr);
@@ -36,23 +37,31 @@ int main(int argc, char **argv){
 	for(i=0; i<FD_SETSIZE;i++){
 		if(FD_ISSET(i,&tmp_set)){
 			if(i==sockfd){
-				accept(sockfd,(struct sockaddr*)&clientaddr,&len);
+				clientsocket = accept(sockfd,(struct sockaddr*)&clientaddr,&len);
 				FD_SET(clientsocket,&sockets);
 			}
-                        else if(i == STDIN_FILENO){
+            else if(i == STDIN_FILENO){
 			  char input[5000];
 			  fgets(input,5000,stdin);
 			  printf("\nSending to client: %s",input);
-			  send(,input,strlen(input)+1,0);
-			  printf("\nEnter your message: ");
-                        }
+			  send(clientsocket,input,strlen(input)+1,0);
+
+			  if(strcmp(input, "quit")==0){
+                close(i);
+                FD_CLR(i,&sockets);
+                return 0;
+              }
+            }
 			else{
 				char line[5000];
 				recv(i,line,5000,0);
 				printf("Got from client: %s\n",line);
-				send(clientsocket,line,strlen(line)+1,0);
-				close(i);
-				FD_CLR(i,&sockets);
+				printf("size: %d",strlen(line));
+				if(strcmp(line, "quit\0")==0){
+                  close(i);
+                  FD_CLR(i,&sockets);
+                  return 0;
+                }
 			}
 		}
 	}
